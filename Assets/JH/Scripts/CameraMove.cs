fe5af638d573e7bd411bf5be2ecea3bffe0ce4d7
Jihoon <35093963+MightyChipmunk@users.noreply.github.com>
2022-07-28 11:15:28 +0900

    에너미 움직임 및 카메라 개선

--- Assets/JH/Scripts/CameraMove.cs
+++ Assets/JH/Scripts/CameraMove.cs
@@ -10,29 +10,49 @@ public class CameraMove : MonoBehaviour
     float angle = 1.0f;
     public float Angle { get { return angle; } set { angle = value; } }
     float lerp = 0;
+
+    Vector3 delta = new Vector3 (0, 2, -4);
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        CamMove();
+    }
+
+    void CamMove()
+    {
         if (Input.GetKey(KeyCode.A))
-        {
             lerp = Mathf.Lerp(lerp, angle, Time.deltaTime * speed);
-            transform.localPosition = new Vector3(lerp, transform.localPosition.y, transform.localPosition.z);
-        }
         else if (Input.GetKey(KeyCode.D))
-        {
             lerp = Mathf.Lerp(lerp, -angle, Time.deltaTime * speed);
-            transform.localPosition = new Vector3(lerp, transform.localPosition.y, transform.localPosition.z);
+        else
+            lerp = Mathf.Lerp(lerp, 0, Time.deltaTime * speed);
+
+        CamBetweenWall();
+    }
+
+    void CamBetweenWall()
+    {
+        delta = new Vector3(lerp, delta.y, delta.z);
+        RaycastHit hit;
+        Vector3 dir = transform.position - transform.parent.position;
+
+        if (Physics.Raycast(transform.parent.position, dir, out hit, delta.magnitude, LayerMask.GetMask("Wall")))
+        {
+            float dist = (transform.parent.position - hit.point).magnitude * 0.8f;
+            transform.localPosition = delta.normalized * dist;
         }
         else
         {
-            lerp = Mathf.Lerp(lerp, 0, Time.deltaTime * speed);
-            transform.localPosition = new Vector3(lerp, transform.localPosition.y, transform.localPosition.z);
+            transform.localPosition = delta;
         }
+
+        if (transform.localPosition.magnitude > delta.magnitude)
+            transform.localPosition = delta;
     }
 }
