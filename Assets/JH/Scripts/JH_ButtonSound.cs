0647ddcc066974a96035aac286a31ca8ccbf2850
Kyounj2 <108376415+Kyounj2@users.noreply.github.com>
2022-08-29 01:59:16 +0900

    0829

--- Assets/JH/Scripts/JH_ButtonSound.cs
+++ Assets/JH/Scripts/JH_ButtonSound.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class JH_ButtonSound : MonoBehaviour
 {
@@ -24,12 +25,12 @@ public class JH_ButtonSound : MonoBehaviour
     public void ChangeScene()
     {
         source.PlayOneShot(buttonSound);
-        GameObject.Find("Start Button").SetActive(false);
+        GameObject.Find("Start Button").GetComponent<Button>().enabled = false;
 
         GameObject.Find("Player").GetComponent<Animator>().SetTrigger("Selected");
         GameObject.Find("Enemy").GetComponent<Animator>().SetTrigger("Selected");
 
-        iTween.ScaleTo(GameObject.Find("Main"), iTween.Hash("x", 0, "y", 0, "z", 0, "time", 0.5f, "easetype", iTween.EaseType.easeInCirc));
+        //iTween.ScaleTo(GameObject.Find("Main"), iTween.Hash("x", 0, "y", 0, "z", 0, "time", 0.5f, "easetype", iTween.EaseType.easeInCirc));
         iTween.MoveTo(cam1, iTween.Hash("z", 0.8f, "time", 1.5f, "easetype", iTween.EaseType.easeInCirc));
         iTween.MoveTo(cam2, iTween.Hash("z", 0.8f, "time", 1.5f, "easetype", iTween.EaseType.easeInCirc));
 
